8ca5e24b7a2b1b4db79c3b360a0fe401224f2b56
rachel490 <ccori12@naver.com>
2022-09-01 01:55:12 +0900

    :sparkles: page number url의 query param으로 넘겨주기 + 이전/다음 페이지 구현

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,25 +1,66 @@
-import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 import * as S from "./footer.styled";
 
 const Footer = () => {
+  const router = useRouter();
+  const {
+    query: { page = 1 },
+  } = router;
+  const [startPage, setStartPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(Number(page));
+
+  const pageChange = (e: React.MouseEvent) => {
+    const pageNum = e.currentTarget.getAttribute("data-page");
+    pageNum && setCurrentPage(Number(pageNum));
+  };
+
+  const prevPage = (e: React.MouseEvent) => {
+    if (startPage === 1) return;
+    setStartPage((prev) => (prev -= 10));
+    setCurrentPage(startPage - 1);
+    router.push({
+      pathname: "/products",
+      query: { page: startPage - 1 },
+    });
+  };
+
+  const nextPage = (e: React.MouseEvent) => {
+    setStartPage((prev) => (prev += 10));
+    setCurrentPage(startPage + 10);
+    router.push({
+      pathname: "/products",
+      query: { page: startPage + 10 },
+    });
+  };
+
   return (
     <S.Wrap data-raw='{"currentPageIndex":1 }' data-total="9">
       <div className="page-wrapper">
-        <a className="icon prev-page" data-page="1">
+        <a className="icon prev-page" onClick={prevPage}>
           <span>이전</span>
         </a>
-        <a className="selected" data-page="1">
-          1
-        </a>
-        <a data-page="2">2</a>
-        <a data-page="3">3</a>
-        <a data-page="4">4</a>
-        <a data-page="5">5</a>
-        <a data-page="6">6</a>
-        <a data-page="7">7</a>
-        <a data-page="8">8</a>
-        <a data-page="9">9</a>
-        <a className="icon next-page" data-page="9">
+        {new Array(10)
+          .fill(startPage)
+          .map((key, idx) => key + idx)
+          .map((pagenum) => {
+            return (
+              <Link
+                href={pagenum === 1 ? "/products" : `?page=${pagenum}`}
+                key={pagenum}
+              >
+                <a
+                  data-page={pagenum}
+                  onClick={pageChange}
+                  className={`${pagenum === currentPage ? "selected" : ""}`}
+                >
+                  {pagenum}
+                </a>
+              </Link>
+            );
+          })}
+        <a className="icon next-page" onClick={nextPage}>
           <span>다음</span>
         </a>
       </div>
