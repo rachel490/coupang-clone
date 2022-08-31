fc1d20c6a4e4dca97861b82bac3ac5cadcaa534c
rachel490 <ccori12@naver.com>
2022-09-01 03:35:56 +0900

    :sparkles: add URL as useQuery dependency

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,29 +1,42 @@
 import styled from "@emotion/styled";
 import axios from "axios";
+import { useRouter } from "next/router";
 import { useQuery } from "react-query";
+
+import Footer from "../src/components/footer/footer";
+import Header from "../src/components/header/header";
 import { ProductItem } from "../src/components/productItem/productItem";
 import { IProductItem } from "../types/product";
 
 export default function ProductListPage() {
-  const { data } = useQuery("products", () =>
-    axios.get(
-      process.env.NEXT_PUBLIC_API_HOST +
-        "/products?offset=0&limit=40&sorter=bestAsc"
-    )
-  );
+  const {
+    query: { page, sorter = "bestAsc" },
+  } = useRouter();
+  const URL =
+    process.env.NEXT_PUBLIC_API_HOST +
+    `/products?offset=5&limit=40&sorter=${sorter}`;
 
-  // console.log(data);
+  const { data } = useQuery(["products",URL], () => axios.get(URL));
 
   return (
-    <Container>
-      {data?.data?.map((item: IProductItem) => (
-        <ProductItem {...item} key={item.id} />
-      ))}
-    </Container>
+    <Wrap>
+      <Header />
+      <Container>
+        {data?.data?.map((item: IProductItem) => (
+          <ProductItem {...item} key={item.id} />
+        ))}
+      </Container>
+      <Footer />
+    </Wrap>
   );
 }
 
+export const Wrap = styled.div`
+  width: 1080px;
+  padding: 20px 0 0 0px;
+`;
 export const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
+  width: 100%;
 `;
