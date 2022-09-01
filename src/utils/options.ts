53bb3e267ef167b4f4505da2e8d715d8c13c33a7
rachel490 <ccori12@naver.com>
2022-09-02 00:20:50 +0900

    :recycle: seperate sort, list, default options

---
+++ src/utils/options.ts
@@ -0,0 +1,15 @@
+export const DEFAULT_OPTION = {
+  sort: "bestAsc",
+  page: 1,
+  list: 12,
+};
+
+export const SORTING_OPTIONS = [
+  { value: "bestAsc", name: "쿠팡 랭킹순" },
+  { value: "salePriceAsc", name: "낮은 가격순" },
+  { value: "salePriceDesc", name: "높은 가격순" },
+  { value: "saleCountDesc", name: "판매량순" },
+  { value: "latestAsc", name: "최신순" },
+];
+
+export const LISTING_OPTIONS = [12, 24];
